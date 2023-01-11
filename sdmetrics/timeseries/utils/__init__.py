2dc146a4370b7f75febff9c4b4c3a80c90e35ff0
Minhao Jin <minhaoj@andrew.cmu.edu>
2023-01-11 10:49:26 -0500

    update new func

--- sdmetrics/timeseries/utils/__init__.py
+++ sdmetrics/timeseries/utils/__init__.py
@@ -1,7 +1,9 @@
 from .distribution_similarity import distribution_similarity
 from .coverage import coverage
+from .pearson_corr import pearson_corr
 
 __all__ = [
     'distribution_similarity',
-    'coverage'
+    'coverage',
+    'pearson_corr'
 ]
