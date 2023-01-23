2d1f14a021f0dbc488b3cf5baaeaea53935da53f
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-01-23 14:54:00 -0500

    single attr coverage dict return

--- sdmetrics/timeseries/fidelity/single_attr_coverage.py
+++ sdmetrics/timeseries/fidelity/single_attr_coverage.py
@@ -14,28 +14,30 @@ class SingleAttrCoverage(TimeSeriesMetric):
 
     @classmethod
     def compute(cls, real_data, synthetic_data, metadata=None,
-                entity_columns=None):
+                entity_columns=None, target=None):
+        if not all(isinstance(s, str) for s in target):
+            raise ValueError(
+                "target has to be a list of strings where each string specifies an attribute column.")
+
         _, entity_columns = cls._validate_inputs(
             real_data, synthetic_data, metadata, entity_columns)
         real_data_attribute, _, _ = \
             cls._load_attribute_feature(real_data, metadata)
         synthetic_data_attribute, _, _ = \
             cls._load_attribute_feature(synthetic_data, metadata)
-        scores = {}
-        for column_name, real_column in real_data.items():
-            if column_name not in metadata['entity_columns'] + metadata['context_columns']:
-                continue
-            if column_name in metadata['fields']:
-                real_column = real_column.to_numpy().reshape(-1, 1)
-                synthetic_column = synthetic_data[column_name].to_numpy(
-                ).reshape(-1, 1)
+        attribute_cols, feature_cols = cls._get_attribute_feature_cols(metadata)
+        for col in target:
+            if col not in attribute_cols:
+                raise ValueError(f"Column {col} is not an attribute.")
 
-                scores[column_name] = coverage(
-                    real_data=real_column,
-                    synthetic_data=synthetic_column,
-                    column_names=[column_name],
-                    data_type=[metadata['fields'][column_name]['type']],
-                    comparison_type='both'
-                )
+        real_columns = real_data[target].to_numpy().reshape(-1, len(target))
+        synthetic_columns = synthetic_data[target].to_numpy(
+        ).reshape(-1, len(target))
 
-        return scores
+        return coverage(
+            real_data=real_columns,
+            synthetic_data=synthetic_columns,
+            column_names=target,
+            data_type=[metadata['fields'][col]['type'] for col in target],
+            comparison_type='both'
+        )
